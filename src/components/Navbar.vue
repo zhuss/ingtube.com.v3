3cf9cf03b8ee8c66ca47349e6d293a35514833f4
zhushunsheng <503305196@qq.com>
2017-05-16 17:01:00 +0800

    入驻

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,7 +4,18 @@
     <ul>
       <li><a :class="active=='index'?'active':''" href="index.html">首页</a></li>
       <li><a :class="active=='about'?'active':''" href="about.html">关于</a></li>
+      <li><a href="javascript:;" @click="dialog = true">入驻</a></li>
     </ul>
+    <div class="dialog" v-if="dialog" @click.self="dialog = false">
+      <h2>入驻映兔视频</h2>
+      <div class="dialog-content">
+        <p>请下载入驻申请表，填写后发送至leyao@ingtube.com，我们会在第一时间回复。</p>
+        <p>表格下载地址：<a href="http://cdn.bunny-tech.com/promotion/static/res/%E6%98%A0%E5%85%94%E8%A7%86%E9%A2%91%E6%B3%A8%E5%86%8C.xlsx">映兔视频注册.xlsx</a></p>
+        <p style="text-align:center; padding-top: 50px;">
+          <button @click="dialog = false">知道了</button>
+        </p>
+      </div>
+    </div>
   </div>
   <!--nav end-->
 </template>
@@ -15,19 +26,24 @@ export default {
       type: String,
       default: 'index'
     }
+  },
+  data() {
+    return {
+      dialog: false
+    }
+
   }
 }
 </script>
 <style scoped>
 .nav {
-  width: 130px;
   position: absolute;
   top: 32px;
   right: 220px;
+  text-align: center;
 }
 
 .nav ul {
-  width: 130px;
   height: 24px;
   line-height: 24px;
 }
@@ -44,8 +60,48 @@ export default {
   color: rgba(255, 255, 255, 1);
 }
 
-.nav ul li:first-child {
-  margin-right: 74px;
+.nav ul li {
+  padding: 0 30px;
+}
+
+.dialog {
+  position: fixed;
+  top: 0;
+  left: 0;
+  background: rgba(0, 0, 0, .9);
+  z-index: 1000;
+  width: 100%;
+  height: 100%;
+  color: #FFF;
+}
+
+.dialog h2 {
+  font-size: 18px;
+  font-weight: normal;
+  height: 100px;
+  line-height: 100px;
+}
+
+.dialog-content {
+  width: 400px;
+  text-align: left;
+  margin: 0 auto;
+}
+
+.dialog-content p {
+  padding: 10px 0;
+}
+
+.dialog-content p a {
+  color: #F8CA03;
+}
+
+.dialog-content button {
+  background: #F8CA03;
+  border: none;
+  width: 100px;
+  height: 30px;
+  border-radius: 30px;
 }
 
 @media screen and (max-width: 750px) {
@@ -58,6 +114,12 @@ export default {
     right: 0;
   }
   .nav ul {
+    display: inline-block;
+    margin: 0 auto;
+  }
+  .dialog-content {
+    width: 300px;
+    text-align: left;
     margin: 0 auto;
   }
 }
